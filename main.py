21f9ba2d649128efcc65bfc4487282f038dd3c7d
shawal-mbalire <mbalireshawal@gmail.com>
2024-07-19 22:01:51 +0300

    first attempt To re iterate with new db model

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-import streamlit as st 
+import streamlit as st
+from utils import PreProcesor, columns
 
-st.write("Hello World")
+import numpy as np
+import pandas as pd
+import joblib
+
+model = joblib.load('model.joblib')
+st.title('Electricity Theft Detection')
+
+columnNumbs: list[int] = [1,2,3,4,5,6,7,8] # list of your features
+columnNames: list[str] = ['2']
+
+def show_prediction():
+    row = np.array(columnNumbs)
+    X = pd.DataFrame([row],columns=columnNames)
+    y = model.predict(X)[0]
+
+st.button('Predict',on_click=show_prediction)
